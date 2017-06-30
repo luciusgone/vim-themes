152799de33e588f2f40e7fe17d10e01f172e67d7
luciusgone <luciusgone@gmail.com>
2017-06-30 16:45:43 +0800

    remove utils.Palette class

--- themes/utils.py
+++ themes/utils.py
@@ -166,26 +166,6 @@ class HiRule(object):
         eol = "\n"
         return g + gf + gb + ga + tf + tb + ta + eol
 
-class Palette(object):
-    def __init__(self, colorset):
-        self.color_set = colorset
-    @property
-    def palette_board(self):
-        global colorset
-        bg_str = self._ansi_esc_str("background")
-        bg = bg_str * (12 * 6 + 2) + "\n"
-        fg = ""
-        for color in colorset:
-            if color != "background":
-                s = self._ansi_esc_str(color)
-                fg += bg_str * 2 + s * 4
-        fg += bg_str * 2 + "\n"
-        pb_str = bg + fg * 2 + bg
-        return pb_str
-    def _ansi_esc_str(self, name):
-        c = self.color_set[name, False]
-        return f"\033[48;5;{c}m \033[m"
-
 class Theme(object):
     def __init__(self, scheme_name, theme_background, terminal_colors, **cs):
         self.scheme_name      = scheme_name
@@ -193,7 +173,6 @@ class Theme(object):
         self.terminal_colors  = terminal_colors
         self.hi_rules         = {}
         self.color_set        = ColorSet(cs, terminal_colors)
-        self.palette          = Palette(self.color_set)
     def add_rules(self, rules):
         for rule in rules:
             hi = HiRule(rule, self.color_set)
@@ -217,4 +196,17 @@ class Theme(object):
         return cr_str
     @property
     def palette_board(self):
-        return self.palette.palette_board
+        global colorset
+        bg_str = self._ansi_esc_str("background")
+        bg = bg_str * (12 * 6 + 2) + "\n"
+        fg = ""
+        for color in colorset:
+            if color != "background":
+                s = self._ansi_esc_str(color)
+                fg += bg_str * 2 + s * 4
+        fg += bg_str * 2 + "\n"
+        pb_str = bg + fg * 2 + bg
+        return pb_str
+    def _ansi_esc_str(self, name):
+        c = self.color_set[name, False]
+        return f"\033[48;5;{c}m \033[m"
