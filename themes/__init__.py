1a14b503598c351bfc88bd8535a1adfde24e9d38
luciusgone <luciusgone@gmail.com>
2017-08-18 22:50:42 +0800

    better structure

--- themes/__init__.py
+++ themes/__init__.py
@@ -1,25 +1,22 @@
-from themes.collection import all_collection
-from themes.rules import all_rules
+from .collection import all_collection
+from .rules import all_rules
 
 for t in all_collection:
-    r = all_collection[t]
-    r.add_rules(all_rules)
+    t.add_rules(all_rules)
 
 def generate():
     # TODO: we can specify a scheme to generate
     for t in all_collection:
-        r = all_collection[t]
-        print(f"generating theme {r.scheme_name}")
-        with open(f"./colors/{r.scheme_name}.vim", "w+") as f:
-            f.write(r.common_rules)
-            f.write(r.rules)
+        print(f"generating theme {t.scheme_name}")
+        with open(f"./colors/{t.scheme_name}.vim", "w+") as f:
+            f.write(t.common_rules)
+            f.write(t.rules)
 
 def print_color_palette():
     # TODO: we can print a specific palette
     for t in all_collection:
-        r = all_collection[t]
-        print(f"{r.scheme_name}:\n")
-        print(r.palette_board)
+        print(f"{t.scheme_name}:\n")
+        print(t.palette_board)
 
 def get_hi_rule(theme, rule):
     # TODO: implement this
--- themes/keywords.py
+++ themes/keywords.py
@@ -1,6 +1,29 @@
-# attribute keyword
+# attribute keywords
 none    = "none"
 bold    = "bold"
 reverse = "reverse"
 
+# color keywords
+foreground = "foreground"
+background = "background"
+selection  = "selection"
+line       = "line"
+comment    = "comment"
+red        = "red"
+orange     = "orange"
+yellow     = "yellow"
+green      = "green"
+aqua       = "aqua"
+blue       = "blue"
+purple     = "purple"
+window     = "window"
 
+colorset  = (foreground, background, selection, line, comment, red,
+        orange, yellow, green, aqua, blue, purple, window)
+
+__ALL__ = (
+    none, bold, reverse,
+    foreground, background, selection, line, comment,
+    red, orange, yellow, green, aqua, blue, purple,
+    window,
+)
